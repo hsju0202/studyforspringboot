92c78ecbd31d19253bf962eb6305fc68268bd661
hsju0202 <hsju0202@gmail.com>
2021-05-02 19:22:01 +0900

    3장

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,42 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts extends BaseTimeEntity {
+
+    @Id //해당 테이블 PK 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK의 생성 규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false) //테이블의 컬럼 기본값 외 변경하고 싶은 옵션이 있으면 사용
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
