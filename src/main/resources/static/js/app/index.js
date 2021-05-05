8203bd8b19b1d4fc83f66b0ce9d117dddb529ea8
hsju0202 <hsju0202@gmail.com>
2021-05-05 23:40:12 +0900

    5장

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,19 +1,19 @@
 var main = {
-    init : function(){
+    init : function () {
         var _this = this;
-        $('#btn-save').on('click', function(){
+        $('#btn-save').on('click', function () {
             _this.save();
         });
 
-        $('#btn-update').on('click', function(){
+        $('#btn-update').on('click', function () {
             _this.update();
         });
 
-        $('#btn-delete').on('click', function(){
+        $('#btn-delete').on('click', function () {
             _this.delete();
-        })
+        });
     },
-    save : function(){
+    save : function () {
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
@@ -24,12 +24,12 @@ var main = {
             type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function(){
+        }).done(function() {
             alert('글이 등록되었습니다.');
             window.location.href = '/';
-        }).fail(function(error){
+        }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     },
@@ -68,6 +68,8 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
-    };
+    }
+
+};
 
 main.init();
